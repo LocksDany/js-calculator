7dc603fce21cb8bf9bc50bd09c90ea7b065ca4bd
Dany <LocksDanny@outlook.com>
2017-09-10 20:33:03 -0300

    v0.2

--- js/main.js
+++ js/main.js
@@ -1,41 +1,91 @@
 const teclado = [
-    7, 8, 9,
-    4, 5, 6,
-    1, 2, 3,
-    0, "=", "/",
-    ".", "x"
+    'C', 'C', '/', 'x',
+    '7', '8', '9', '+',
+    '4', '5', '6', '-',
+    '1', '2', '3', '=',
+    '0', '.'
 ];
 
 let botones = "";
+let resultados = "0";
+let resultadoAnterior = 0;
 
-for(var i = 0; i<teclado.length;i++){
-    botones += "<button class='btn btn-default' onclick='btnNum(" + teclado[i] + ")'>" + teclado[i];
-    switch(i){
-        case 2:
-        case 5:
-        case 8:
-        case 11:
-            botones += "</button><br>";
-            break;
+const giveFunc = (input) => {
+    let html = "";
+    switch(input){
         default:
-            botones += "</button>";
+            html = "btnNum(\"" + input + "\")";
+            return html;
+            break;
+        case 'C':
+            html = "delAll()' id='delAll";
+            return html;
+            break;
+        case '+':
+            html = "sumNum()' id='sum";
+            return html;
+            break;
+        case '=':
+            html = "equalsNum()' id='equals";
+            return html;
+            break;
     }
 }
 
-const btnNum = (num) => {
+for(var i = 0; i<teclado.length;i++){
+    botones += "<button class='btn btn-default' onclick='"+ giveFunc(teclado[i]) +"'>" + teclado[i] + "</button>";
+}
+
+const btnNum = (input) => {
     if(resultados.length < 12){
+        console.log(input);
+        
         if(resultados == "0"){
-            resultados = num;
+            resultados = input;
             $('#resultados').html(resultados);
         } else {
-            resultados += num;
+            resultados += input;
             $('#resultados').html(resultados);
         }
     }
 }
 
-let resultados = "0";
+const sumNum = () => {
+    if(resultadoAnterior > 0){
+        equalsNum();
+        resultadoAnterior = parseInt(resultados);
+    } else if (resultados != "0"){
+        resultadoAnterior = parseInt(resultados);
+        resultados = "0";
+        $('#resultados').html(resultados);
+    }
+}
 
+const equalsNum = () => {
+    if(resultadoAnterior > 0){
+        resultados = resultadoAnterior + parseInt(resultados);
+        resultados += "";
+        $('#resultados').html(resultados);
+        resultadoAnterior = 0;
+    }
+}
+
+const del = () => {
+    let a = resultados.split('');
+    a.pop();
+    resultados = a.join('');
+    $('#resultados').html(resultados);
+    if(resultados.length < 1){
+        resultados = "0";
+        $('#resultados').html(resultados);
+    }
+}
+
+const delAll = () => {
+    resultados = "0";
+    resultadoAnterior = 0;
+    $('#resultados').html(resultados);
+}
 
 $(document).ready(function(){
     $('#botones').html(botones);
@@ -44,19 +94,14 @@ $(document).ready(function(){
        if(Number.isInteger(parseFloat(event.key))){
            btnNum(event.key);
        }
+        
         else if (event.keyCode == 8) {
-           let a = resultados.split('');
-           a.pop();
-           resultados = a.join('');
-           $('#resultados').html(resultados);
-           
-           if(resultados.length < 1){
-               resultados = "0";
-               $('#resultados').html(resultados);
-           }
+           del();
        }
-        else if(event.key == "+"){
-            
+        switch(event.key){
+            case "+":
+                sumNum();
+                break;
         }
         
     });
